fd0a049602815f0a320b96dd14cc5012fa4fb6b4
Elsherbinyomarayman2004 <Elsherbinyomarayman2004@gmail.com>
2026-09-02 12:13:03 +0300

    Add project files.

---
+++ assigment03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assigment03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
