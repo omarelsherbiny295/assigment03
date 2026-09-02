d59e6d07c46f3c8ebb8e6785f67e0d7bac1ed8e6
Elsherbinyomarayman2004 <Elsherbinyomarayman2004@gmail.com>
2026-09-02 12:29:25 +0300

    question2

--- assigment03/Program.cs
+++ assigment03/Program.cs
@@ -7,16 +7,40 @@
 
             #region question1
 
-            int pages = 464;
-            bool isavalibale = true;
+            //int pages = 464;
+            //bool isavalibale = true;
 
-            if (pages>300 && isavalibale ==true)
-            {
-                Console.WriteLine("you can borrow this book");
-            }
+            //if (pages>300 && isavalibale ==true)
+            //{
+            //    Console.WriteLine("you can borrow this book");
+            //}
 
 
             #endregion
+
+            #region question2
+            string title = "refactoring";
+
+            switch (title)
+            {
+
+                case "clean code" :
+                    Console.WriteLine("great choice ");
+                    break;
+
+                case "refactoring":
+                    Console.WriteLine("nice pickk ");
+                    break;
+
+                default:
+                    Console.WriteLine("NEVER HEARD OF IT ");
+                    break;
+
+            }
+            #endregion
+
+
+
 
         }
     }
